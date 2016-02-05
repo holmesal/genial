f61b2003061eb73bf7a6140a1958a0ab59215259
Alonso Holmes <hello@alonso.io>
2016-02-05 14:45:22 -0800

    updated server to use heroku auto-assigned ports

--- server.js
+++ server.js
@@ -5,19 +5,7 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import {Schema} from './data/schema';
 
-const APP_PORT = 3000;
-const GRAPHQL_PORT = 8080;
-
-// Expose a GraphQL endpoint
-var graphQLServer = express();
-graphQLServer.use('/', graphQLHTTP({
-  graphiql: true,
-  pretty: true,
-  schema: Schema,
-}));
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
-));
+const APP_PORT = process.env.PORT || 8080;
 
 // Serve the Relay app
 var compiler = webpack({
@@ -38,12 +26,19 @@ var compiler = webpack({
 });
 var app = new WebpackDevServer(compiler, {
   contentBase: '/public/',
-  proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
+  //proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
   publicPath: '/js/',
   stats: 'errors-only'
 });
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
+// GraphQL endpoint
+app.use('/graphql', graphQLHTTP({
+  graphiql: true,
+  pretty: true,
+  schema: Schema,
+}));
 app.listen(APP_PORT, () => {
   console.log(`App is now running on http://localhost:${APP_PORT}`);
+  console.log(`GraphQL endpoint is at http://localhost:${APP_PORT}/graphql`);
 });
