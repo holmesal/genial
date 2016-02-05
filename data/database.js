8844905f17d23e2e2b0976a372d1d0a007e1308b
Alonso Holmes <hello@alonso.io>
2016-02-05 15:06:38 -0800

    different urls for local and heroku dbs

--- data/database.js
+++ data/database.js
@@ -2,7 +2,9 @@ import faker from 'faker';
 import Sequelize from 'sequelize';
 import _ from 'lodash';
 
-var sequelize = new Sequelize('postgres://btyuwemjytpdjx:SnG3_aA7tmQkplL9LFygAdAQcL@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d3ssevji1t6kb3');
+console.info(process.env.NODE_ENV);
+let dbUrl = process.env.NODE_ENV === 'production' ? 'postgres://btyuwemjytpdjx:SnG3_aA7tmQkplL9LFygAdAQcL@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d3ssevji1t6kb3' : 'postgres://postgres:postgres@localhost:5432/genial';
+var sequelize = new Sequelize(dbUrl);
 
 // Model types
 var Post = sequelize.define('post', {
--- js/components/App.js
+++ js/components/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Widget list</h1>
+        <h1>Post list</h1>
         <ul>
           {this.props.viewer.widgets.edges.map(edge =>
             <li key={edge.node.id}>{edge.node.name} (ID: {edge.node.id})</li>
@@ -18,17 +18,17 @@ class App extends React.Component {
 
 export default Relay.createContainer(App, {
   fragments: {
-    viewer: () => Relay.QL`
-      fragment on User {
-        widgets(first: 10) {
-          edges {
-            node {
-              id,
-              name,
-            },
-          },
-        },
-      }
-    `,
+    //viewer: () => Relay.QL`
+    //  fragment on User {
+    //    widgets(first: 10) {
+    //      edges {
+    //        node {
+    //          id,
+    //          name,
+    //        },
+    //      },
+    //    },
+    //  }
+    //`,
   },
 });
