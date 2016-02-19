08081fed25260cf5b5144074b97469f9148bcad9
Alonso Holmes <hello@alonso.io>
2016-02-19 15:08:37 -0800

    using ssl

--- dist/database.js
+++ dist/database.js
@@ -1,5 +1,10 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getPosts = exports.getPost = exports.Post = undefined;
+
 var _faker = require('faker');
 
 var _faker2 = _interopRequireDefault(_faker);
@@ -15,27 +20,35 @@ var _lodash2 = _interopRequireDefault(_lodash);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var dbUrl = process.env.NODE_ENV === 'production' ? 'postgres://btyuwemjytpdjx:SnG3_aA7tmQkplL9LFygAdAQcL@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d3ssevji1t6kb3' : 'postgres://postgres:postgres@localhost:5432/genial';
-var sequelize = new _sequelize2.default(dbUrl);
+var sequelize = new _sequelize2.default(dbUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 // Model types
-var Post = sequelize.define('post', {
+var Post = exports.Post = sequelize.define('post', {
   content: _sequelize2.default.TEXT
 });
 
-sequelize.sync({ force: true }).then(function () {
-  Post.create({
-    content: _faker2.default.lorem.paragraph()
-  }).then(function (post) {
-    console.info(post.get());
-  });
-});
-
-module.exports = {
-  getPost: function getPost(id) {
-    return Post.findById(id);
-  },
-  getPosts: function getPosts() {
-    return Post.findAll();
-  },
-  Post: Post
+var getPost = exports.getPost = function getPost(id) {
+  return Post.findById(id);
 };
+var getPosts = exports.getPosts = function getPosts() {
+  return Post.findAll();
+};
+
+// Sync
+if (process.env.NODE_ENV === 'production' || true) {
+  sequelize.sync().then(function () {
+    return console.info('connected and synced!');
+  });
+} else {
+  sequelize.sync().then(function () {
+    return console.info('[DEV DB] connected and synced!');
+  });
+  //sequelize.sync({force: true}).then(() => {
+  //  //bootstrap(User, Podcast)
+  //});
+}
--- src/database.js
+++ src/database.js
@@ -3,24 +3,27 @@ import Sequelize from 'sequelize';
 import _ from 'lodash';
 
 let dbUrl = process.env.NODE_ENV === 'production' ? 'postgres://btyuwemjytpdjx:SnG3_aA7tmQkplL9LFygAdAQcL@ec2-54-225-199-245.compute-1.amazonaws.com:5432/d3ssevji1t6kb3' : 'postgres://postgres:postgres@localhost:5432/genial';
-var sequelize = new Sequelize(dbUrl);
+var sequelize = new Sequelize(dbUrl, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 
 // Model types
-var Post = sequelize.define('post', {
+export const Post = sequelize.define('post', {
   content: Sequelize.TEXT
 });
 
-sequelize.sync({force: true}).then(() => {
-  Post.create({
-    content: faker.lorem.paragraph()
-  }).then((post) => {
-    console.info(post.get())
-  });
-});
+export const getPost = (id) => Post.findById(id);
+export const getPosts = () => Post.findAll();
 
-module.exports = {
-  getPost: (id) => Post.findById(id),
-  getPosts: () => Post.findAll(),
-  Post
-};
-
+// Sync
+if (process.env.NODE_ENV === 'production' || true) {
+  sequelize.sync().then(() => console.info('connected and synced!'));
+} else {
+  sequelize.sync().then(() => console.info('[DEV DB] connected and synced!'));
+  //sequelize.sync({force: true}).then(() => {
+  //  //bootstrap(User, Podcast)
+  //});
+}
